f8ef69118ad8d523b4b2e5f9acf506d11e407261
Christiane Brüggemann <129063452+nane1992@users.noreply.github.com>
2024-12-07 00:18:02 +0100

    Day 6

---
+++ Tag06/part1.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+with open("data.txt", "r") as file:
+    grid = [list(line.strip()) for line in file]
+mymap = np.array(grid)
+print(mymap)
+
+# finding the starting position:
+num_rows, num_cols = mymap.shape
+start = np.where(mymap == "^")
+row, col = start[0][0], start[1][0]
+print(f"Guard's starting position: ({row}, {col})")
+visited_positions = set()
+visited_positions.add((row, col))
+direction = "up"
+
+# guard track:
+while True:
+    current_position = mymap[row, col]
+    print(f"Checking position ({row}, {col}): {current_position}")
+
+    if current_position == "#":
+        print(f"Encountered # at ({row}, {col}), changing direction.")
+        if direction == "up":
+            direction = "right"
+            row += 1 # to stay below #
+        elif direction == "right":
+            direction = "down"
+            col -=1
+        elif direction == "down":
+            direction = "left"
+            row -= 1
+        elif direction == "left":
+            direction = "up"
+            col += 1
+        else:
+            break
+    try:   
+        if direction == "up":
+            row -= 1  # Move up
+        elif direction == "right":
+            col += 1  # Move right
+        elif direction == "down":
+            row +=1 # Move down
+        elif direction == "left":
+            col -= 1 # move left
+        
+        if mymap[row, col] != "#":
+            visited_positions.add((row, col))
+            
+    except IndexError:
+        print(f"Guard existed at ({row}, {col}).")
+        break
+    
+
+print(f"Total moves: {len(visited_positions)}")
+
